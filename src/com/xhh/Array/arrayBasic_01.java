83a2c4e64cbd6d7a691f33c14e4bf10be478e4c7
lktalented <715466676@qq.com>
2018-11-28 18:01:44 +0800

    Initial commit

---
+++ src/com/xhh/Array/arrayBasic_01.java
@@ -0,0 +1,31 @@
+package com.xhh.Array;
+
+/**
+ * 数组基础
+ * luokai
+ * 2018/11/28 0028 下午 4:44
+ */
+public class arrayBasic_01 {
+    public static void main(String[] args) {
+        int[]arr = new int[10];
+        for (int i = 0;i<arr.length;i++){
+            arr[i]=i;
+            System.out.println(arr[i]);
+        }
+
+        int[]scores = new int[]{100,80,60};
+        for (int i = 0;i<scores.length;i++ ){
+            System.out.println(scores[i]);
+        }
+        for (int score:scores){
+            System.out.println(score);
+        }
+        scores[0]=99;
+
+        for (int i = 0; i < scores.length; i++) {
+            System.out.println(scores[i]);
+        }
+
+
+    }
+}
